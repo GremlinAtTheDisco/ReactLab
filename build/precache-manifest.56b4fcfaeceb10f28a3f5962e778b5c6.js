0e57fa1499ff76a5c3adc234275bbc1799cd8586
GremlinAtTheDisco <skyesme@gmail.com>
2019-04-08 13:28:22 +0200

    init commit

---
+++ build/precache-manifest.56b4fcfaeceb10f28a3f5962e778b5c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d6fdc9da9cf274052b1e",
+    "url": "/static/js/main.c35e7b16.chunk.js"
+  },
+  {
+    "revision": "e0ea3310b3f4d66d0fc2",
+    "url": "/static/js/2.b4290454.chunk.js"
+  },
+  {
+    "revision": "d6fdc9da9cf274052b1e",
+    "url": "/static/css/main.b820c815.chunk.css"
+  },
+  {
+    "revision": "0045f46d26005774733ab425678fa3a1",
+    "url": "/index.html"
+  }
+];
---
+++ src/App.js
@@ -0,0 +1,105 @@
+import React, { Component } from 'react'
+import Header from './components/ui/Header/Header'
+import { fetchBooks } from './utils/api';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { _title: '', _author: '', _status: '' };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChangeTitle = this.handleChangeTitle.bind(this);
+    this.handleChangeAuthor = this.handleChangeAuthor.bind(this);
+    this.handleBooks = this.handleBooks.bind(this);
+  }
+
+  handleSubmit(event) {
+    alert('foo bar ' + this.state._title + ' ' + this.state._author);
+    event.preventDefault();
+  }
+
+  handleChangeTitle(event) {
+    this.setState({ _title: event.target.value });
+  }
+
+  handleChangeAuthor(event) {
+    this.setState({ _author: event.target.value });
+  }
+  handleBooks(result) {
+    this.setState({ _status: result.status });
+    console.log(result.status);
+  }
+  componentDidMount() {
+    fetchBooks().then(this.handleBooks);
+    
+  }
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <div className="container">
+          <div className="row form-section">
+            <form className="book-form col-6" onSubmit={this.handleSubmit}>
+              <legend>Lägg till dina favoritböcker</legend>
+              <div className="form-group">
+                <input
+                  type="text"
+                  className="form-control"
+                  id="title"
+                  aria-describedby="title"
+                  placeholder="Lägg till titel"
+                  value={this.state._title}
+                  onChange={this.handleChangeTitle}
+                />
+
+                <input
+                  type="text"
+                  className="form-control"
+                  id="author"
+                  rows="3"
+                  data-gramm="true"
+                  data-txt_gramm_id="63b74fb6-c7e4-7f0e-0c1f-438d47ac87a0"
+                  data-gramm_id="63b74fb6-c7e4-7f0e-0c1f-438d47ac87a0"
+                  data-gramm_editor="true"
+                  placeholder="Lägg till författare"
+                  value={this.state._author}
+                  onChange={this.handleChangeAuthor}
+                />
+              </div>
+              <button
+                type="submit"
+                className="btn btn-primary btn-lg btn-block"
+              >
+                Skicka
+              </button>
+            </form>
+          </div>
+        </div>
+        <div className="display-books">
+          <div className="container">
+            <div className="col-12">
+              <ul className="list-group">
+                <li className="list-item list-group-item d-flex align-items-center">
+                  <strong className="title">Titel</strong>
+
+                  <div className="author">Författare</div>
+
+                  <div className="buttons">
+                    <button type="button" className="btn btn-success">
+                      Editera
+                    </button>
+                    <button type="button" className="btn btn-danger">
+                      Ta bort
+                    </button>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
